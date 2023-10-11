e646ecb0774cb17d2431c9bf115b47d4354eaa68
nikhilthadani <nikhil_thadani@persistent.com>
2023-10-11 20:30:40 +0530

    Vercel settings applied

--- jobproject/settings.py
+++ jobproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gq5q11bk$ms&t7ttvrp9de#hlr5az#+)fgqf^mec9n*jfn3%r#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
